3c0e225bf15d90df1202da83092baf1da4a02a10
Yabadaba <allenbao01@gmail.com>
2021-11-30 19:15:27 -0800

    fake done with magic8ball class

--- FinalProject.java
+++ FinalProject.java
@@ -4,8 +4,19 @@
 // A draft
 // Inputs that break code:
 
+import java.util.Scanner;
+
 public class FinalProject {
    public static void main(String[] args) {
       Magic8Ball myBall = new Magic8Ball("responses.txt");
+      
+      Scanner keyboard = new Scanner(System.in);
+      
+      int playTimes = 0;
+      
+      while(true) {
+         ++playTimes;
+         break;
+      }
    }
 }
--- Magic8Ball.java
+++ Magic8Ball.java
@@ -5,22 +5,53 @@
 // Inputs that break code:
 
 import java.util.Scanner;
+import java.io.File;
 import java.util.Random;
 import java.util.ArrayList;
 
 public class Magic8Ball {
+   private ArrayList<String> responses;
+   private Random random;
+   
    // Constructor
    public Magic8Ball(String fileName) {
-      ArrayList<String> responses = new ArrayList<String>();
-      Random random = new Random();
-      Scanner fileReader = new Scanner(fileName);
+      responses = new ArrayList<String>();
+      
+      random = new Random();
+      
+      File fileToRead = new File(fileName);
+      Scanner fileReader = new Scanner(fileToRead);
       
       while(fileReader.hasNext()) {
          responses.add(fileReader.nextLine());
       }
+      
+      // debug loop
+      for (String s : responses) {
+         System.out.println(s);
+      }
+      
    }
    
    public String getResponse() {
-      return null;
+      // return a random response
+      return responses.get(random.nextInt(responses.size()));
+   }
+   
+   public String getKarma(int playTimes) {
+      switch (playTimes) {
+         case 1:
+            return "Great Misfortune";
+         case 2:
+            return "Misfortune";
+         case 3:
+            return "Moderate Fortune";
+         case 4:
+   			Return "Rising Fortune"
+   		case 5: 
+   			return "Good Fortune";
+   		default: 
+   			return "Great Fortune";
+      }
    }
 }
