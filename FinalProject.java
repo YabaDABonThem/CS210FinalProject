8939e5743a6f1736783800a5063acd85c25cefea
Yabadaba <allenbao01@gmail.com>
2021-11-30 18:31:05 -0800

    first commit

---
+++ FinalProject.java
@@ -0,0 +1,11 @@
+// Allen Bao
+// 11/30/21
+// Final Project First Draft
+// A draft
+// Inputs that break code:
+
+public class FinalProject {
+   public static void main(String[] args) {
+      Magic8Ball myBall = new Magic8Ball("responses.txt");
+   }
+}
---
+++ Magic8Ball.java
@@ -0,0 +1,14 @@
+// Allen Bao
+// 11/30/21
+// Final Project First Draft
+// The 8Ball class
+// Inputs that break code:
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Magic8Ball {
+   public Magic8ball(String fileName) {
+      
+   }
+}
