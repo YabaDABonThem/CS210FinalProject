86bc4b9b20bb8cb8fcc44833ecca2076baf498cc
Yabadaba <allenbao01@gmail.com>
2021-12-08 16:32:52 -0800

    Done?

--- FinalProject.java
+++ FinalProject.java
@@ -1,7 +1,7 @@
 // Allen Bao
 // 11/30/21
-// Final Project First Draft
-// A draft
+// Final Project
+// Creates Magic 8 ball to answer the user's questions
 // Inputs that break code: none
 
 import java.util.Scanner;
@@ -9,20 +9,27 @@ import java.io.IOException;
 
 public class FinalProject {
    public static void main(String[] args) throws IOException {
+      // Create new Magic8Ball and pass it the file name of the responses file.
       Magic8Ball myBall = new Magic8Ball("responses.txt");
       
+      // Create Scanner to get user input
       Scanner keyboard = new Scanner(System.in);
       
+      // Create loop variable to count the amount of questions asked
       int playTimes = 0;
       
+      // infinite loop
       while(true) {
+         // increase our playcount
          ++playTimes;
+         // ask the user for a question
          System.out.println("Enter a yes or no question, or say \"quit\" to quit: ");
-         if (keyboard.nextLine().equals("quit")) {
+         if (keyboard.nextLine().equals("quit")) { // exit loop when user says to quit
             break;
          }
+         // Print the Magic 8 Ball's response and the user's karma score
          System.out.println(myBall.getResponse());
-         System.out.println(myBall.getKarma(playTimes));
+         System.out.println("Your fortune outlook is: " + myBall.getKarma(playTimes) + ". Ask more questions for a better fortune. ");
       }
    }
 }
--- Magic8Ball.java
+++ Magic8Ball.java
@@ -12,18 +12,24 @@ import java.util.ArrayList;
 import java.io.IOException;
 
 public class Magic8Ball {
+   // instatiate fields
    private ArrayList<String> responses;
    private Random random;
    
    // Constructor
    public Magic8Ball(String fileName) throws IOException {
+      // Create ArrayList to hold all the responses stored in the specified file
       responses = new ArrayList<String>();
       
+      // Create random object to generate random numbers
       random = new Random();
       
+      // Create a File object to read from
       File fileToRead = new File(fileName);
+      // Create a Scanner that reads from the previously created file
       Scanner fileReader = new Scanner(fileToRead);
       
+      // Go through every line of the file and add each line to the ArrayList
       while(fileReader.hasNext()) {
          responses.add(fileReader.nextLine());
       }
@@ -31,12 +37,12 @@ public class Magic8Ball {
    }
    
    public String getResponse() {
-      // return a random response
+      // return a random response from responses ArrayList
       return responses.get(random.nextInt(responses.size()));
    }
    
    public String getKarma(int playTimes) {
-      switch (playTimes) {
+      switch (playTimes) { // Return a better fortune the more times played
          case 1:
             return "Great Misfortune";
          case 2:
